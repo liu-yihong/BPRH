ad9de904bde572f7d737b90297f9e6303aa521ce
liu-yihong <lfa_1@126.com>
2020-07-09 19:31:24 -0500

    bhpH

--- bprH_gpu.py
+++ bprH_gpu.py
@@ -1,6 +1,7 @@
 import pickle
 import pandas as pd
 import numpy as np
+import cupy
 from random import choice
 from tqdm import tqdm, trange
 from livelossplot import PlotLosses
@@ -11,7 +12,7 @@ def adv_index(list_to_index, list_to_match):
 
 
 def sigmoid(z):
-    return 1 / (1 + np.exp(-z))
+    return cupy.reciprocal(1 + cupy.exp(-z))
 
 
 def load_model(load_path):
@@ -28,7 +29,7 @@ def save_model(model_instance, saved_path):
 
 class bprH(object):
 
-    def __init__(self, dim, omega, rho, lambda_u, lambda_v, lambda_b, gamma, num_iter=200, random_state=None):
+    def __init__(self, dim=10, omega=1, rho=1, lambda_u=0.5, lambda_v=0.1, lambda_b=0.1, gamma=0.001, num_iter=200, random_state=None):
         """
         Initializing class instance bprH
         :param dim: the dimension of latent vector
@@ -69,6 +70,9 @@ class bprH(object):
         self.train_data = None
         self.test_data = None
 
+        self.I_u_t = dict()
+        self.I_u_a = dict()
+
     def auxiliary_target_correlation(self, X, y=None):
         """
         Calculate auxiliary-target correlation C for every user and each types of auxiliary action
@@ -80,10 +84,10 @@ class bprH(object):
         print("Calculate auxiliary-target correlation")
         target_action = 'P'
         auxiliary_action = ['V']
-        C_u = dict()
+        alpha_u = dict()
         user_set_bar = tqdm(self.user_list)
         for u in user_set_bar:
-            C_u[u] = dict()
+            alpha_u[u] = dict()
             I_t_u = set(X[(X.UserID == u) & (X.Action == target_action)].ItemID)
             # TODO: filtered item set
             for x in auxiliary_action:
@@ -97,15 +101,16 @@ class bprH(object):
                 # hence, C_u_X here is 1 / omega because alpha_u = omega * C_u_X
                 C_u_X = 2 * C_u_at * C_u_ta / (C_u_ta + C_u_at) if C_u_ta + C_u_at != 0 else (1 / self.omega)
                 # set final alpha_u to 1 if C_u_ta + C_u_at == 0
-                C_u[u][x] = C_u_X
+                alpha_u[u][x] = C_u_X
+                alpha_u[u]['alpha'] = self.omega * self.rho * C_u_X
 
-        temp = pd.DataFrame.from_dict(C_u, orient='index')
+        #temp = pd.DataFrame.from_dict(C_u, orient='index')
         # We have only one auxiliary action 'V'
-        temp['alpha'] = self.omega * self.rho * temp.V
-        alpha_u = temp
-        del temp
-        alpha_u.reset_index(inplace=True)
-        alpha_u.columns = ['UserID', 'V', 'alpha']
+        #temp['alpha'] = self.omega * self.rho * temp.V
+        #alpha_u = temp
+        #del temp
+        #alpha_u.reset_index(inplace=True)
+        #alpha_u.columns = ['UserID', 'V', 'alpha']
         return alpha_u
 
     def itemset_coselection(self, saved_path, X, y=None):
@@ -134,6 +139,15 @@ class bprH(object):
             pickle.dump(S, f, pickle.HIGHEST_PROTOCOL)
 
         return S
+
+    def build_itemset_for_user(self):
+        print("Build I_u_t, I_u_a")
+        user_set_bar = tqdm(self.user_list)
+        for u in user_set_bar:
+            self.I_u_t[u] = set(self.train_data[(self.train_data.UserID == u) & (self.train_data.Action == 'P')].ItemID)
+            self.I_u_a[u] = set(self.train_data[(self.train_data.UserID == u) & (
+                    self.train_data.Action == 'V')].ItemID)
+
 
     def fit(self, X, eval_X, original_item_list, original_user_list, y=None,
             saved_path='data/item-set-coselection.pkl', coselection=False, plot_metric=False):
@@ -167,22 +181,26 @@ class bprH(object):
 
         # Initialization of User and Item Matrices
         if self.random_state is not None:
-            np.random.seed(self.random_state)
+            cupy.random.seed(self.random_state)
         else:
-            np.random.seed(0)
+            cupy.random.seed(0)
 
-        self.U = np.random.normal(size=(self.num_u, self.dim + 1))
-        self.V = np.random.normal(size=(self.dim + 1, self.num_i))
+        self.U = cupy.random.normal(size=(self.num_u, self.dim + 1))
+        self.V = cupy.random.normal(size=(self.dim + 1, self.num_i))
         self.U[:, -1] = 1
         # estimation is U dot V
-        self.estimation = np.dot(self.U, self.V)
+        self.estimation = cupy.dot(self.U, self.V)
 
         # plot loss
         if plot_metric:
             groups = {'Precision@K': ['Precision@5', 'Precision@10'], 'Recall@K': ['Recall@5', 'Recall@10']}
             plot_losses = PlotLosses(groups=groups)
 
+        # build I_u_t, I_u_a
+        self.build_itemset_for_user()
+
         # Start Iteration
+        all_item = set(self.item_list)
         with trange(self.num_iter) as t:
             for index in t:
                 # Description will be displayed on the left
@@ -194,7 +212,7 @@ class bprH(object):
 
                 # build I
                 # uniformly sample a item i from I_u_t
-                I_u_t = set(self.train_data[(self.train_data.UserID == u) & (self.train_data.Action == 'P')].ItemID)
+                I_u_t = self.I_u_t[u]
                 if len(I_u_t) != 0:
                     i = choice(sorted(I_u_t))
                     # build I = I_u_t cap S_i
@@ -209,8 +227,7 @@ class bprH(object):
                     I = set()
 
                 # build J, since we only have one auxiliary action, we follow the uniform sampling
-                I_u_oa = set(self.train_data[(self.train_data.UserID == u) & (
-                        self.train_data.Action == 'V')].ItemID) - I_u_t  # TODO: optimize this
+                I_u_oa = self.I_u_a[u] - I_u_t
                 if len(I_u_oa) != 0:
                     j = choice(sorted(I_u_oa))
                     if coselection:
@@ -224,7 +241,7 @@ class bprH(object):
                     J = set()
 
                 # build K
-                I_u_n = set(self.item_list) - I_u_t - I_u_oa
+                I_u_n = all_item - I_u_t - I_u_oa
                 if len(I_u_n) != 0:
                     k = choice(sorted(I_u_n))
                     # build K
@@ -240,33 +257,33 @@ class bprH(object):
 
                 # calculate intermediate variables
                 # get specific alpha_u
-                spec_alpha_u = self.alpha_u[self.alpha_u.UserID == u].alpha.values[0]
+                spec_alpha_u = self.alpha_u[u]['alpha']
 
                 U_u = self.U[u, :-1]
                 # get r_hat_uIJ and r_hat_uJK
-                r_hat_uI = np.average(self.estimation[u, sorted(I)]) if len(
-                    I) != 0 else 0
-                r_hat_uJ = np.average(self.estimation[u, sorted(J)]) if len(
-                    J) != 0 else 0
-                r_hat_uK = np.average(self.estimation[u, sorted(K)]) if len(
-                    K) != 0 else 0
+                r_hat_uI = cupy.average(self.estimation[u, sorted(I)]) if len(
+                    I) != 0 else cupy.array([0])
+                r_hat_uJ = cupy.average(self.estimation[u, sorted(J)]) if len(
+                    J) != 0 else cupy.array([0])
+                r_hat_uK = cupy.average(self.estimation[u, sorted(K)]) if len(
+                    K) != 0 else cupy.array([0])
 
                 r_hat_uIJ = r_hat_uI - r_hat_uJ
                 r_hat_uJK = r_hat_uJ - r_hat_uK
                 # get V_bar_I, V_bar_J, V_bar_K
-                V_bar_I = np.average(self.V[:-1, sorted(I)], axis=1) if len(
-                    I) != 0 else np.zeros(
+                V_bar_I = cupy.average(self.V[:-1, sorted(I)], axis=1) if len(
+                    I) != 0 else cupy.zeros(
                     shape=(self.dim,))
-                V_bar_J = np.average(self.V[:-1, sorted(J)], axis=1) if len(
-                    J) != 0 else np.zeros(
+                V_bar_J = cupy.average(self.V[:-1, sorted(J)], axis=1) if len(
+                    J) != 0 else cupy.zeros(
                     shape=(self.dim,))
-                V_bar_K = np.average(self.V[:-1, sorted(K)], axis=1) if len(
-                    K) != 0 else np.zeros(
+                V_bar_K = cupy.average(self.V[:-1, sorted(K)], axis=1) if len(
+                    K) != 0 else cupy.zeros(
                     shape=(self.dim,))
                 # get b_I, b_J, b_K
-                b_I = np.average(self.V[-1, sorted(I)]) if len(I) != 0 else 0
-                b_J = np.average(self.V[-1, sorted(J)]) if len(J) != 0 else 0
-                b_K = np.average(self.V[-1, sorted(K)]) if len(K) != 0 else 0
+                b_I = cupy.average(self.V[-1, sorted(I)]) if len(I) != 0 else cupy.array([0])
+                b_J = cupy.average(self.V[-1, sorted(J)]) if len(J) != 0 else cupy.array([0])
+                b_K = cupy.average(self.V[-1, sorted(K)]) if len(K) != 0 else cupy.array([0])
 
 
                 # get derivatives and update
@@ -276,7 +293,7 @@ class bprH(object):
                          sigmoid(- r_hat_uJK) * (V_bar_J - V_bar_K)
                 dR_dUu = 2 * self.lambda_u * U_u
                 # update U_u = U_u + gamma * (df_dUu - dR_dUu)
-                norm_nabula_U_u = np.linalg.norm((df_dUu - dR_dUu), ord=2)
+                norm_nabula_U_u = cupy.linalg.norm((df_dUu - dR_dUu), ord=2)
                 self.U[u, :-1] += self.gamma * (df_dUu - dR_dUu)
 
                 if len(I) != 0:
@@ -286,7 +303,7 @@ class bprH(object):
                     df_dVi = df_dbi * U_u
                     dR_dVi = 2 * self.lambda_v * V_bar_I / len(I)
 
-                    norm_nabula_Vi = np.linalg.norm((df_dVi - dR_dVi), ord=2)
+                    norm_nabula_Vi = cupy.linalg.norm((df_dVi - dR_dVi), ord=2).item()
 
                     # update V_i = V_i + gamma * (df_dVi - dR_dVi)
                     self.V[:-1, sorted(I)] += self.gamma * (df_dVi - dR_dVi)[:, None]  # trick: transpose here
@@ -302,7 +319,7 @@ class bprH(object):
                     df_dVj = df_dbj * U_u
                     dR_dVj = 2 * self.lambda_v * V_bar_J / len(J)
 
-                    norm_nabula_Vj = np.linalg.norm((df_dVj - dR_dVj), ord=2)
+                    norm_nabula_Vj = cupy.linalg.norm((df_dVj - dR_dVj), ord=2).item()
 
                     # update V_j = V_j + gamma * (df_dVj - dR_dVj)
                     self.V[:-1, sorted(J)] += self.gamma * (df_dVj - dR_dVj)[:, None]  # trick: transpose here
@@ -318,7 +335,7 @@ class bprH(object):
                     df_dVk = df_dbk * U_u
                     dR_dVk = 2 * self.lambda_v * V_bar_K / len(K)
 
-                    norm_nabula_Vk = np.linalg.norm((df_dVk - dR_dVk), ord=2)
+                    norm_nabula_Vk = cupy.linalg.norm((df_dVk - dR_dVk), ord=2).item()
 
                     # update V_k = V_k + gamma * (df_dVk - dR_dVk)
                     self.V[:-1, sorted(K)] += self.gamma * (df_dVk - dR_dVk)[:, None]  # trick: transpose here
@@ -328,11 +345,11 @@ class bprH(object):
                     norm_nabula_Vk = 0
 
                 # calculate loss
-                f_Theta = np.log(sigmoid(r_hat_uIJ / spec_alpha_u)) + np.log(sigmoid(r_hat_uJK))
-                regula = self.lambda_u * np.linalg.norm(U_u, ord=2) + self.lambda_v * (
-                        (np.linalg.norm(V_bar_I, ord=2) if len(I) != 0 else 0) + (
-                            np.linalg.norm(V_bar_J, ord=2) if len(J) != 0 else 0) + (
-                            np.linalg.norm(V_bar_K, ord=2)) if len(K) != 0 else 0) + self.lambda_b * (
+                f_Theta = cupy.log(sigmoid(r_hat_uIJ / spec_alpha_u)) + cupy.log(sigmoid(r_hat_uJK))
+                regula = self.lambda_u * cupy.linalg.norm(U_u, ord=2) + self.lambda_v * (
+                        (cupy.linalg.norm(V_bar_I, ord=2) if len(I) != 0 else 0) + (
+                            cupy.linalg.norm(V_bar_J, ord=2) if len(J) != 0 else 0) + (
+                            cupy.linalg.norm(V_bar_K, ord=2)) if len(K) != 0 else 0) + self.lambda_b * (
                                      (b_I if len(I) != 0 else 0) ** 2 + (b_J if len(J) != 0 else 0) ** 2 + (
                                  b_K if len(K) != 0 else 0) ** 2)
                 bprh_loss = f_Theta - regula
@@ -345,7 +362,7 @@ class bprH(object):
                                                                             ground_truth=self.test_data,
                                                                             K=10)
                 # update estimation
-                self.estimation = np.dot(self.U, self.V)
+                self.estimation = cupy.dot(self.U, self.V)
                 # Postfix will be displayed on the right,
                 # formatted automatically based on argument's datatype
                 t.set_postfix(loss=bprh_loss, precision_5=precision_5, recall_5=recall_5, precision_10=precision_10,
@@ -362,9 +379,9 @@ class bprH(object):
 
     def predict_estimation(self, user_to_predict, item_to_predict=None):
         if item_to_predict is None:
-            return np.dot(self.U[adv_index(self.user_original_id_list, user_to_predict), :], self.V)
+            return cupy.dot(self.U[adv_index(self.user_original_id_list, user_to_predict), :], self.V)
         else:
-            return np.dot(self.U[adv_index(self.user_original_id_list, user_to_predict), :],
+            return cupy.dot(self.U[adv_index(self.user_original_id_list, user_to_predict), :],
                           self.V[:, adv_index(self.item_original_id_list, item_to_predict)])
 
     def recommend(self, user_to_recommend=None, K=5):
@@ -376,31 +393,35 @@ class bprH(object):
 
         estimated_pref = self.predict_estimation(user_to_predict=self.user_original_id_list)
 
-        # build the dict that user's purchased item
-        user_purchased_item = dict()
         # build the rec list for users
-        user_rec_list = []
+        user_rec_dict = dict()
 
         # print("Build User's Purchased Item Dict & Rec List")
         # user_set_bar = tqdm(user_to_recommend)
         for u in user_to_recommend:
-            user_purchased_item[u] = set(
-                self.train_data[(self.train_data.UserID == u) & (self.train_data.Action == 'P')].ItemID)
+            user_rec_dict[u] = set()
             est_pref_of_u = estimated_pref[u, :]
             #
-            est_pref_sort_index = est_pref_of_u.argsort()[::-1]
+            est_pref_sort_index = est_pref_of_u.argsort()[::-1].get()
             rec_item_cnt = 0
-            index_cnt = 0
-            while rec_item_cnt < K:
-                if est_pref_sort_index[index_cnt] in user_purchased_item[u]:
-                    index_cnt += 1
-                else:
-                    user_rec_list.append([u, est_pref_sort_index[index_cnt]])
-                    index_cnt += 1
+            for item_id in est_pref_sort_index:
+                if rec_item_cnt == K-1:
+                    break
+                if item_id not in self.I_u_t[u]:
+                    user_rec_dict[u].add(item_id)
                     rec_item_cnt += 1
 
-        self.user_purchased_item = user_purchased_item
-        return pd.DataFrame(user_rec_list, columns=['UserID', 'ItemID'])
+            #rec_item_cnt = 0
+            #index_cnt = 0
+            #while rec_item_cnt < K:
+            #    if est_pref_sort_index[index_cnt] in self.I_u_t[u]:
+            #        index_cnt += 1
+            #    else:
+            #        user_rec_list.append([u, est_pref_sort_index[index_cnt]])
+            #        index_cnt += 1
+            #        rec_item_cnt += 1
+
+        return user_rec_dict
 
     def scoring(self, ground_truth, K=5, user_to_eval=None, y=None):
         """
@@ -416,22 +437,25 @@ class bprH(object):
 
         user_to_eval = sorted(set(user_to_eval))
         # get top K recommendation list
-        rec_list = self.recommend(user_to_recommend=user_to_eval, K=K)
+        user_rec_dict = self.recommend(user_to_recommend=user_to_eval, K=K)
         scoring_list = []
         # clean ground truth
         ground_truth = ground_truth[ground_truth.Action == 'P']
         # begin iteration
         for u in user_to_eval:
-            rec_list_for_user_u = set(rec_list[rec_list.UserID == u].ItemID)
+            rec_list_for_user_u = user_rec_dict[u]
             ground_truth_for_user_u = set(ground_truth[ground_truth.UserID == u].ItemID)
             precision_K_for_u = len(rec_list_for_user_u.intersection(ground_truth_for_user_u)) / K
             recall_K_for_u = len(rec_list_for_user_u.intersection(ground_truth_for_user_u)) / len(
                 ground_truth_for_user_u) if len(ground_truth_for_user_u) != 0 else np.nan
             scoring_list.append([u, precision_K_for_u, recall_K_for_u])
-        scoring_list = pd.DataFrame(scoring_list, columns=['UserID', 'Precision@' + str(K), 'Recall@' + str(K)])
-        precision_K = scoring_list.mean()['Precision@' + str(K)]
-        recall_K = scoring_list.mean()['Recall@' + str(K)]
-
+        #scoring_list = pd.DataFrame(scoring_list, columns=['UserID', 'Precision@' + str(K), 'Recall@' + str(K)])
+        #precision_K = scoring_list.mean()['Precision@' + str(K)]
+        #recall_K = scoring_list.mean()['Recall@' + str(K)]
+        scoring_list = np.array(scoring_list)
+        scoring_average = scoring_list.mean(axis=0)
+        precision_K = scoring_average[1]
+        recall_K = scoring_average[2]
         return scoring_list, precision_K, recall_K
 
     def get_params(self, deep=True):
